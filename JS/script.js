cac1577c4413cb05813865959f2e30afe986b0c9
meniaclol <arijuslaurin@gmail.com>
2022-10-11 10:16:01 +0200

    Updated file structure & class names

--- JS/script.js
+++ JS/script.js
@@ -5,8 +5,8 @@ const feedback = document.querySelector(".PasswordResult");
 
 function check_form() {
   event.preventDefault();
-  const username = document.getElementById("username").value;
-  const username2 = document.getElementById("username2").value;
+  const password = document.getElementById("password").value;
+  const password2 = document.getElementById("password2").value;
   const feedback = document.querySelector(".feedback");
   const AccountName = document.getElementById("createName").value;
   const letter = /[a-z]/;
@@ -15,28 +15,28 @@ function check_form() {
   const specialChars = /\W|_/g;
 
   if (
-    username.length < 13 ||
-    username != username2 ||
-    !letter.test(username) ||
-    !number.test(username) ||
-    !upper.test(username) ||
-    !specialChars.test(username)
+    password.length < 13 ||
+    password != password2 ||
+    !letter.test(password) ||
+    !number.test(password) ||
+    !upper.test(password) ||
+    !specialChars.test(password)
   ) {
-    if (username.length < 13) {
+    if (password.length < 13) {
       feedback.textContent = "Make sure password is atleast 13 letters";
       if (!feedback.classList.contains("error")) {
         feedback.classList.add("class", "error");
       }
       return false;
     }
-    if (username != username2) {
+    if (password != password2) {
       feedback.textContent = "Make sure both passwords match";
       if (!feedback.classList.contains("error")) {
         feedback.classList.add("class", "error");
       }
       return false;
     }
-    if (!letter.test(username)) {
+    if (!letter.test(password)) {
       feedback.textContent =
         "Make sure password includes atleast one lowercase letter";
       if (!feedback.classList.contains("error")) {
@@ -44,14 +44,14 @@ function check_form() {
       }
       return false;
     }
-    if (!number.test(username)) {
+    if (!number.test(password)) {
       feedback.textContent = "Make sure password includes atleast one number";
       if (!feedback.classList.contains("error")) {
         feedback.classList.add("class", "error");
       }
       return false;
     }
-    if (!upper.test(username)) {
+    if (!upper.test(password)) {
       feedback.textContent =
         "Make sure password includes atleast one uppercase letter";
       if (!feedback.classList.contains("error")) {
@@ -59,7 +59,7 @@ function check_form() {
       }
       return false;
     }
-    if (!specialChars.test(username)) {
+    if (!specialChars.test(password)) {
       feedback.textContent =
         "Make sure password includes atleast one special character";
       if (!feedback.classList.contains("error")) {
@@ -72,7 +72,7 @@ function check_form() {
     feedback.textContent = "That password is valid, redirecting you to login page. . .";
     if (!feedback.classList.contains("success")) {
       feedback.classList.add("class", "success");
-      localStorage.setItem("password", username);
+      localStorage.setItem("password", password);
       localStorage.setItem("username", AccountName);
     }
     setTimeout(function(){
@@ -86,14 +86,14 @@ function check_form() {
 
 
 function Login (){
-  const LoginPass = document.getElementById("username3").value;
+  const LoginPass = document.getElementById("passwordActual").value;
   const LoginUsername = document.getElementById("usernameActual").value;
   event.preventDefault();
   if(LoginPass == localStorage.getItem("password") && LoginUsername == localStorage.getItem("username")){
     feedback.textContent = "You are logged in! Redirecting you to home page. . ."
 
     setTimeout(function(){
-      document.location.href = "index.html";
+      document.location.href = "../index.html";
     }, 3000);
   // }else if(LoginPass != localStorage.getItem("password")){
   //   feedback.textContent = "Invalid account"
